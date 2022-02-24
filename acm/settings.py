a93cdb65fd4671ef65e7f65e8ec2b43196ae5182
harshjadon9 <62889318+harshjadon9@users.noreply.github.com>
2022-02-24 16:00:08 +0530

    Update settings.py

--- acm/settings.py
+++ acm/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'acm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [os.path.join(BASE_DIR, "static")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
